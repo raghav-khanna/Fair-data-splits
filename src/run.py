dfd3767e75835628387b5907cc937c8b6feb4722
Pranav Chatur <pranavchatur@gmail.com>
2024-05-05 16:09:46 +0530

    Add src/run.py

--- src/run.py
+++ src/run.py
@@ -1,8 +1,10 @@
-
-def Allfunctions (name: str) -> int:
-    print('Import all functions to this file and run only this file: ', end='')
+def Allfunctions(name: str) -> int:
+    print(
+        'Import all functions to this file and run only this file: ', end = ''
+    )
     print(name)
     return 1
+
 
 if __name__ == "__main__":
     Allfunctions('run.py')
