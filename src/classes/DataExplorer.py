56b7a94e52354e3be3e7e3d749f6fc612604dcf0
Pranav Chatur <pranavchatur@gmail.com>
2024-08-10 11:14:51 +0530

    Add DataWranglerClass (#5)
    * Create fairMetric class
    
    * Format code
    
    * Format code again
    
    * Add DataWranglerClass and LogHandler, Rename ClassifierClass to DataClassiferClass, Construct mock csv and store it in < root>/data
    
    * Resolve bug
    
    * Add function to change yN, tF columns to 10
    
    * Format code
    
    * Add binary encoding function in comments
    
    * Organize run.py
    
    ---------
    
    Co-authored-by: Raghav Khanna <raghav.29.khanna@gmail.com>

---
+++ src/classes/DataExplorer.py
@@ -0,0 +1,12 @@
+
+'''
+
+TODO:
+
+Data Explorer:
+1. Correlation values
+2. Draw visualizations
+3. Principle Component Analysis (PCA)
+
+
+'''
--- src/run.py
+++ src/run.py
@@ -1,19 +1,37 @@
-from classes.Classifier import ClassifierClass
+from classes.DataClassifier import DataClassifierClass
+from classes.DataWrangler import DataWranglerClass
+# import numpy as np
 
 
-def Allfunctions(name: str) -> int:
+def all_functions(name: str) -> int:
     # print('Import all functions to this file and run only this file: ', end = '')
     # print(name)
 
-    trainSet = [[1, 1, 1], [2, 1, 1], [1, 2, 1], [-1, -1, 0], [-1, -2, 0], [-2, -1, 0]]
-    testSet = [[2, 2, 1], [-2, -2, 0], [0, 0, 0], [3, 1, 0], [3, 1, 0]]
-    clf = ClassifierClass(trainSet, testSet)
+    dataset = DataWranglerClass('<abs-path>/Fair-data-splits/data/student.csv', cols_to_encode = None)
+    # dataset.convert_yn_tf_to_binary(cols_to_encode=['age'])
+    dataset.convert_yn_tf_to_binary(cols_to_encode = None)
+    df = dataset.get_processed_dataframe()
+    print(df)
+
+    # Data Exploring here
+
+    # ls1 = df['name'].values.tolist();ls2 = df['hired'].values.tolist()
+    # print(ls1);print(ls2)
+    # cor = np.corrcoef(ls1, ls2)
+    # print(cor)
+
+    # Data Splitting here
+
+    train_set = [[1, 1, 1], [2, 1, 1], [1, 2, 1], [-1, -1, 0], [-1, -2, 0], [-2, -1, 0]]
+    test_set = [[2, 2, 1], [-2, -2, 0], [0, 0, 0], [3, 1, 0], [3, 1, 0]]
+    clf = DataClassifierClass(train_set, test_set)
     results = clf.evaluate(k = 2)
     print(results)
     clf.performance()
 
+    # Classifier Performance here
     return 1
 
 
 if __name__ == "__main__":
-    Allfunctions('run.py')
+    all_functions('run.py')
