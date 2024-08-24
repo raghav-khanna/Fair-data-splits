ad9fc2b9799710f05b7ba24690c228785258339c
Raghav Khanna <raghav.29.khanna@gmail.com>
2024-08-24 20:02:24 +0530

    Add random data split and balance calculation

--- src/classes/DataSplit.py
+++ src/classes/DataSplit.py
@@ -1,16 +1,51 @@
 import random
 import pandas as pd
+import time
+import logging
 
+logging.getLogger('requests').setLevel(logging.ERROR)
+logging.basicConfig(level=logging.INFO, format="%(levelname)s:%(message)s:\n") # Comment this line to stop showing the messages
 
 class DataSplit:
     # check inbuilt sklearn datasplit function as well
 
-    def __init__(self, dataset: pd.DataFrame) -> None:
-        self.train_set = dataset.loc[0:19]
-        self.test_set = dataset.loc[20:26]
+    def __init__(self, dataset: pd.DataFrame, sensitive_attribute_column: str, sensitive_attribute_value: int) -> None:
+        logging.info('********** Enter DataSplit constructor **********')
+        self.__trainSet: pd.DataFrame = pd.DataFrame()
+        self.__testSet: pd.DataFrame = pd.DataFrame()
+        self.__dataset: pd.DataFrame = dataset
+        self.__sensitive_attribute_column: str = sensitive_attribute_column
+        self.__sensitive_attribute_value: int = sensitive_attribute_value
 
-    def __randomlySplit(dataset: [], trainTestRatio: float = 0.1) -> tuple:
-        testSetSize: int = len(dataset) * trainTestRatio
-        testSet: [] = random.sample(dataset, testSetSize)
-        trainSet: [] = dataset - testSet
-        return trainSet, testSet
+    def random_split(self, split_ratio: float = 0.1, random_state: int = int(time.time())) -> tuple:
+        logging.info("*****Random Splitting*****")
+        dataset = self.__dataset
+        test_set = dataset.sample(frac = split_ratio, random_state = random_state)
+        train_set = dataset.drop(test_set.index)
+        balance_for_split = self.__calculate_balance_for_split((train_set, test_set))
+        return train_set, test_set, balance_for_split
+
+    def __calculate_balance_for_split(self, split: tuple) -> float:
+        logging.info("*****Calculating balance for split*****")
+        train_set, test_set = split
+        dataset_ratio: float = len(self.__dataset[self.__dataset[self.__sensitive_attribute_column] == self.__sensitive_attribute_value]) / len(self.__dataset)
+        train_set_ratio: float = len(train_set[train_set[self.__sensitive_attribute_column] == self.__sensitive_attribute_value]) / len(train_set)
+        test_set_ratio: float = len(test_set[test_set[self.__sensitive_attribute_column] == self.__sensitive_attribute_value]) / len(test_set)
+        if dataset_ratio == 0:
+            logging.error("Dataset ratio is zero")
+            return 0
+        elif train_set_ratio == 0:
+            logging.debug("Train set ratio is zero")
+            train_set_balance: float = 0
+            return 0
+        elif test_set_ratio == 0:
+            logging.debug("Test set ratio is zero")
+            test_set_balance: float = 0
+            return 0
+        test_set_balance: float = min(test_set_ratio / dataset_ratio, dataset_ratio / test_set_ratio)
+        train_set_balance: float = min(train_set_ratio / dataset_ratio, dataset_ratio / train_set_ratio)
+        logging.info(f"Train set balance: {train_set_balance}")
+        logging.info(f"Test set balance: {test_set_balance}")
+        return min(train_set_balance, test_set_balance)
+
+
